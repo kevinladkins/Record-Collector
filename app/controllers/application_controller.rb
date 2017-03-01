f291b75d1dae2fd849f117571994d658c4a7d415
Kevin Adkins <kevinladkins@gmail.com>
2017-03-01 18:35:53 +0000

    setting up basic functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
-require './config/environment'
+require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-  configue do
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
   end
 
 end

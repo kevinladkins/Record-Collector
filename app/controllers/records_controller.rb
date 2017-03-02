13adff40334696c5eee9a1f5f07eb858bbee1967
Kevin Adkins <kevinladkins@gmail.com>
2017-03-02 00:21:07 +0000

    built record navigation

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,24 @@ require_relative './application_controller.rb'
 require_relative './users_controller.rb'
 
 class RecordsController < ApplicationController
+
+  get '/' do
+    erb :'records/index'
+  end
+
+  get '/records/:slug' do
+    @record = Record.find_by_slug(params[:slug])
+    erb :'records/show'
+  end
+
+  get 'records/new' do
+    erb :'records/new'
+  end
+
+  post 'records/new' do
+    record = Record.create(params)
+    redirect "/records/#{record.slug}"
+  end
+
+
 end

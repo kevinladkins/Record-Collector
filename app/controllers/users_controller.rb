b85a1a51401ea0e36caa022c152d09775478cf33
Kevin Adkins <kevinladkins@gmail.com>
2017-03-02 19:06:04 +0000

    building user/show routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,36 @@
 class UsersController < ApplicationController
 
   get '/users/:slug' do
+    if !logged_in?
+      redirect '/login'
     @user = User.find_by_slug(params[:slug])
     erb :'users/index'
   end
+
+  get 'users/:slug/records' do
+    if !logged_in?
+      redirect '/login'
+    else
+      @user = current_user
+      erb :'users/show/by_record.erb'
+    end
+  end
+
+  get 'users/:slug/artists' do
+    if !logged_in?
+      redirect '/login'
+    else
+      @user = current_user
+      erb :'users/show/by_artist.erb'
+    end
+  end
+
+  get 'users/:slug/labels' do
+      erb :'users/show/by_artist.erb'
+  end
+
+
+
+
 
 end

8302121ab88d2da131843f47fca0c21effee0d50
Kevin Adkins <kevinladkins@gmail.com>
2017-03-04 19:54:22 +0000

    reorganized App and User Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,57 +18,9 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  get '/login' do
-    if logged_in?
-      redirect "/users/#{current_user.slug}"
-    else
-    erb :'/users/login'
-    end
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/users/#{user.slug}"
-    else
-      flash[:message] = "Incorrect username or password."
-      redirect '/login'
-    end
-  end
-
-  get '/signup' do
-    if logged_in?
-      redirect "/users/#{current_user.slug}"
-    else
-    erb :'/users/signup'
-    end
-  end
-
-  post '/signup' do
-    if params[:username] == "" || params[:password] == "" || params[:name] == "" || params[:email] == ""
-      flash[:message] = "Please complete all fields."
-      redirect '/signup'
-    elsif User.find_by(username: params[:username], email: params[:email])
-      redirect '/login'
-    else
-    user = User.create(params)
-    session[:user_id] = user.id
-    redirect "users/#{user.slug}"
-    end
-  end
 
   get '/error' do
     erb :error
-  end
-
-  get '/logout' do
-    if !logged_in?
-      redirect '/'
-    else
-      session.clear
-      redirect '/'
-    end
   end
 
   helpers do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,46 @@ class UsersController < ApplicationController
 
   use Rack::Flash
 
+  get '/login' do
+    if logged_in?
+      redirect "/users/#{current_user.slug}"
+    else
+    erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.slug}"
+    else
+      flash[:message] = "Incorrect username or password."
+      redirect '/login'
+    end
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect "/users/#{current_user.slug}"
+    else
+    erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:name] == "" || params[:email] == ""
+      flash[:message] = "Please complete all fields."
+      redirect '/signup'
+    elsif User.find_by(username: params[:username], email: params[:email])
+      redirect '/login'
+    else
+    user = User.create(params)
+    session[:user_id] = user.id
+    redirect "users/#{user.slug}"
+    end
+  end
+
   get '/users/:slug' do
     verify_user
     @user = current_user
@@ -37,8 +77,13 @@ class UsersController < ApplicationController
     redirect "/users/#{params[:slug]}"
   end
 
-
-
-
+  get '/logout' do
+    if !logged_in?
+      redirect '/'
+    else
+      session.clear
+      redirect '/'
+    end
+  end
 
 end

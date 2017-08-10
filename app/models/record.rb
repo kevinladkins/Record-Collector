8a7a78af854f94b93c7cf84a3c7119635c54e917
github username <kevinladkins@gmail.com>
2017-08-10 19:44:00 -0400

    Build responsive styles for mobile

--- app/models/record.rb
+++ app/models/record.rb
@@ -9,6 +9,7 @@ class Record < ActiveRecord::Base
   has_many :users, through: :record_users
   belongs_to :label
   belongs_to :artist
+  after_create :set_default_cover
 
   def cover_upload=(upload)
     file = upload[:tempfile]
@@ -21,6 +22,13 @@ class Record < ActiveRecord::Base
       f.write(upload[:tempfile].read)
     end
     self.cover = "/uploads/#{upload[:filename]}"
+  end
+
+  def set_default_cover
+    if !self.cover
+      self.cover = "https://s3.us-east-2.amazonaws.com/#{ENV['AWS_S3_BUCKET_NAME']}/default_cover.jpg"
+      self.save
+    end
   end
 
 
